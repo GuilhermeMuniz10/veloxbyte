87a6aca426688f271e77497cd300123e93596ca9
gabialbertassi <gabialbertassi01@gmail.com>
2024-10-07 16:33:11 -0300

    subindo

---
+++ src/pageinicial/index.jsx
@@ -0,0 +1,46 @@
+import './index.scss'
+
+export default function App(){
+    return(
+        <div className='tudo'>
+
+            <div className='cabecario'>
+
+                <img src="/assets/images/logovelox.png" alt="logovelox" className='logo'/>
+
+                <ul>
+                  <a href="#" className='Link'>Quem Somos</a>
+                  <a href="#" className='Link1'>Premiações</a>
+                  <a href="#" className='Link2'>Projetos</a>
+                  <a href="#" className='Link3'>Localização</a>
+               </ul>
+
+            </div>
+
+            <div className='part1'>
+                <img src="/assets/images/imgfundo.png" alt=""  className='fundo'/>
+
+                <div class="textDiv">
+                <h1 className='h1'>Inovação á sua Empresa</h1>
+        <p><i id="spaceText" class="fas fa-search"></i>A Velox Byte é uma empresa de tecnologia e informática comprometida em oferecer soluções inovadoras e eficientes para o mundo digital.</p>
+            </div>
+
+                
+                  
+
+                  
+
+                    <img src="/assets/images/imgemp.png" alt="" />
+                </div>
+
+            </div>
+
+        
+
+
+
+
+
+
+    )
+}
