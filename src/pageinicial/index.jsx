6cef6f1b513a98a5a95b697aeeb57979586dae73
guilgermaria <dudaguilger9@gmail.com>
2024-10-25 18:16:07 -0300

    subindo veloxbyte

--- src/pageinicial/index.jsx
+++ src/pageinicial/index.jsx
@@ -18,22 +18,42 @@ export default function App(){
             </div>
 
             <div className='part1'>
-                <img src="/assets/images/imgfundo.png" alt=""  className='fundo'/>
-
-                <div class="textDiv">
-                <h1 className='h1'>Inovação á sua Empresa</h1>
-        <p><i id="spaceText" class="fas fa-search"></i>A Velox Byte é uma empresa de tecnologia e informática comprometida em oferecer soluções inovadoras e eficientes para o mundo digital.</p>
-            </div>
+                <img className='fundo' src="/assets/images/imgfundo.png" alt=""/>
 
                 
-                  
+                    <div className='texto-secao1'>
+                        <h1>Inovação á sua Empresa</h1>
+                        <p >A Velox Byte é uma empresa de <br /> tecnologia e informática comprometida <br /> em oferecer soluções inovadoras e <br /> eficientes para o mundo digital.</p>
+                    </div>
+                
+                
+                    <img className='empre' src="/assets/images/imgemp.png" alt="" />
+            </div>
+            <div className='part2'>
 
-                  
-
-                    <img src="/assets/images/imgemp.png" alt="" />
+                <div className='qdq1'>
+                 <img src="/assets/images/img1.png" alt="" />
+                 <h4 className='premiacoes'>Premiações</h4>
+                 <p className='txt1'>É com grande satisfação que anunciamos que a VeloxByte conquistou o Prêmio de Excelência em Tecnologia deste ano! Agradeço a toda a equipe pelo trabalho árduo e pelo impacto positivo que tivemos no setor. Estamos ansiosos para as próximas conquistas!</p>
                 </div>
 
+                <div className='qdq2'>
+                 <img src="/assets/images/img2.png" alt="" />
+                 <h4 className='gestao'>Gestão</h4>
+                 <p className='txt2'>Na Velox Byte,nossa gestão é centrada em inovação e eficiência. Com líderes experientes e uma equipe colaborativa, buscamos continuamente melhorar nossos processos e resultados. Estamos comprometidos com o crescimento e sucesso de nossos colaboradores e preparados para enfrentar novos desafios e aproveitar oportunidades futuras.</p>
+                </div>
+
+                <div className='qdq2'>
+                 <img src="/assets/images/img1.png" alt="" />
+                 <h4 className='gestao'>Projetos</h4>
+                 <p className='txt3'>Somos especializados no desenvolvimento de sites e plataformas para jovens empreendedores e autônomos.
+                Clique no botão abaixo e seja direcionado a um dos nossos sites de projeto!</p>
+                <button className='botao' href="">Clique aqui!</button>
+                </div>
             </div>
+            </div>
+
+            
 
         
 
