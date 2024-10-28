645fe06410d646ff021ab5aeac757a2a3ed74f8c
guilgermaria <158223104+guilgermaria@users.noreply.github.com>
2024-10-27 21:25:14 -0300

    subindo velox

--- src/pageinicial/index.jsx
+++ src/pageinicial/index.jsx
@@ -4,63 +4,89 @@ export default function App(){
     return(
         <div className='tudo'>
 
-            <div className='cabecario'>
+<div className="paginicio">
 
-                <img src="/assets/images/logovelox.png" alt="logovelox" className='logo'/>
 
-                <ul>
-                  <a href="#" className='Link'>Quem Somos</a>
-                  <a href="#" className='Link1'>Premiações</a>
-                  <a href="#" className='Link2'>Projetos</a>
-                  <a href="#" className='Link3'>Localização</a>
-               </ul>
+<nav className='links'>
 
-            </div>
+   <img src="/assets/images/oi.png" alt="Descrição da Imagem" class="imagem-opaca" />
 
+   <ul>
+      <li><a href="#" className='Link'>Quem Somos</a></li>
+      <li><a href="#" className='Link'>Premiações</a></li>
+      <li><a href="#" className='Link'>Projetos</a></li>
+      <li><a href="#" className='Link'>Localização</a></li>
+   </ul>
+   
+</nav>
+<div class="separador"></div>
+
+</div>
+
+        
             <div className='part1'>
-                <img className='fundo' src="/assets/images/imgfundo.png" alt=""/>
+                <img className='fundo' src="/assets/images/ui.png" alt=""/>
 
                 
                     <div className='texto-secao1'>
-                        <h1>Inovação á sua Empresa</h1>
-                        <p >A Velox Byte é uma empresa de <br /> tecnologia e informática comprometida <br /> em oferecer soluções inovadoras e <br /> eficientes para o mundo digital.</p>
+                        <h1 className='h1'>Inovação á sua Empresa</h1>
+                        <p className='v'>A Velox Byte é uma empresa de <br /> tecnologia e informática comprometida <br /> em oferecer soluções inovadoras e <br /> eficientes para o mundo digital.</p>
                     </div>
                 
                 
                     <img className='empre' src="/assets/images/imgemp.png" alt="" />
-            </div>
-            <div className='part2'>
 
-                <div className='qdq1'>
-                 <img src="/assets/images/img1.png" alt="" />
-                 <h4 className='premiacoes'>Premiações</h4>
-                 <p className='txt1'>É com grande satisfação que anunciamos que a VeloxByte conquistou o Prêmio de Excelência em Tecnologia deste ano! Agradeço a toda a equipe pelo trabalho árduo e pelo impacto positivo que tivemos no setor. Estamos ansiosos para as próximas conquistas!</p>
-                </div>
+                    <div class="separador2"></div>
 
-                <div className='qdq2'>
-                 <img src="/assets/images/img2.png" alt="" />
-                 <h4 className='gestao'>Gestão</h4>
-                 <p className='txt2'>Na Velox Byte,nossa gestão é centrada em inovação e eficiência. Com líderes experientes e uma equipe colaborativa, buscamos continuamente melhorar nossos processos e resultados. Estamos comprometidos com o crescimento e sucesso de nossos colaboradores e preparados para enfrentar novos desafios e aproveitar oportunidades futuras.</p>
-                </div>
-
-                <div className='qdq2'>
-                 <img src="/assets/images/img1.png" alt="" />
-                 <h4 className='gestao'>Projetos</h4>
-                 <p className='txt3'>Somos especializados no desenvolvimento de sites e plataformas para jovens empreendedores e autônomos.
-                Clique no botão abaixo e seja direcionado a um dos nossos sites de projeto!</p>
-                <button className='botao' href="">Clique aqui!</button>
-                </div>
-            </div>
             </div>
 
-            
+
+            <div className='part3'>
+  <div className='ft'>
+    <img className='equipe' src="/assets/images/novo.png" alt="" />
+  </div>
+
+  <div className='part3-1'>
+    <div className='missao'>
+      <h3>MISSÃO</h3>
+      <div className='linha'></div> 
+      <p className='p'>
+      A missão da Velox Byte é transformar desafios tecnológicos em oportunidades de crescimento para nossos clientes, oferecendo soluções inovadoras e eficazes. Especializados em Tecnologia da Informação, capacitamos jovens empreendedores e autônomos com ferramentas que impulsionam seus negócios na era da informação. Comprometidos com a excelência, buscamos entender as necessidades dos clientes e criar soluções personalizadas que promovam eficiência e agilidade. Nossa visão é ser um parceiro estratégico, antecipando demandas do mercado e equipando nossos clientes para um futuro repleto de oportunidades e conquistas.
+      </p>
+    </div>
+ 
+  </div>
+</div>
+
+<div className="page">
+      <div className="card">
+        <img src="/assets/images/img2.png" alt="Premiações" className="card-image" />
+        <h3>Premiações</h3>
+        <p>
+        Na Velox Byte, as premiações refletem a excelência que buscamos. Nossa gestão, focada na inovação, destaca aqueles que se sobressaem, incentivando o progresso. Com líderes qualificados e uma equipe alinhada, celebramos conquistas que reforçam nosso compromisso com o desenvolvimento coletivo. 
+        </p>
+      </div>
+
+      <div className="card">
+        <img src="/assets/images/img1.png" alt="Gestão" className="card-image" />
+        <h3>Gestão</h3>
+        <p>
+        A nossa gestão é fundamentada na inovação e na eficiência, elementos essenciais que guiam cada uma de nossas ações. Com uma equipe colaborativa e líderes experientes, estamos sempre em busca de maneiras de aprimorar nossos processos e resultados, garantindo que estejamos na vanguarda do setor.</p>
+      </div>
+
+      <div className="card">
+        <img src="/assets/images/m.png" alt="Projetos" className="card-image" />
+        <h3>Projetos</h3>
+        <p>
+        Na Velox Byte, somos especialistas em desenvolver sites e plataformas sob medida para jovens empreendedores e autônomos, proporcionando soluções que impulsionam seus negócios. Clique no botão abaixo e explore um dos nossos sites de projeto!
+        </p>
+        <button className="card-button">Clique aqui!</button>
+      </div>
+    </div>
+          
+            </div>
 
         
-
-
-
-
-
 
     )
 }
